f00582232ef3cb537ae6a57aa610a52ee52e789e
Rachna Yadav <rachna.tjr@gmail.com>
2023-11-20 13:00:04 +0530

    api calling

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { YOUTUBE_VIDEOS_API } from '../utils/constant';
 
 const VideoContainer = () => {
+
+useEffect(()=>{
+    getVideos();
+
+},[])
+const getVideos=async()=>{
+    const data=await fetch(YOUTUBE_VIDEOS_API);
+    const json=await data.json();
+    console.log(json);
+}
+
   return (
     <div>VideoContainer</div>
   )
 }
-
 export default VideoContainer
---
+++ src/utils/constant.js
@@ -0,0 +1,5 @@
+
+const GOOGLE_API_KEY="AIzaSyDnjtN9-HZbYVdTSwD480568kEWJqrM5mM";
+
+export const YOUTUBE_VIDEOS_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+GOOGLE_API_KEY;
+
